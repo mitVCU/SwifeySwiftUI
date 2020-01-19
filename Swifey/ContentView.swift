48e17b8d59bc240c8fe8b17dec1fd22f850748f9
Mit Amin <aminm2@mymail.vcu.edu>
2020-01-18 20:06:57 -0500

    Initial Commit

---
+++ Swifey/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swifey
+//
+//  Created by Mit Amin on 1/18/20.
+//  Copyright © 2020 Mit Amin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
