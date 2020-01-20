c1e238269b365ba6538c33ba337b51098ee7de39
Mit Amin <aminm2@mymail.vcu.edu>
2020-01-19 23:52:12 -0500

    Authentication by email is set up 	Sign in page built 	Sign Up page built 	Error handling and displaying built 	Tie back to firebase

--- Swifey/ContentView.swift
+++ Swifey/ContentView.swift
@@ -9,13 +9,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var session: SessionStore
+    
+    func getUser() {
+        session.listen()
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        Group {
+            if session.session != nil {
+                Text("Welcome back user!")
+                Button(action: session.signOut) {
+                    Text("Sign Out")
+                }
+            } else {
+                AuthView()
+            }
+        }.onAppear(perform: getUser)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(SessionStore())
     }
 }
